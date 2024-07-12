2936fca91659018c543c4297c4dedfb34b60c192
João Carlos <joao.gcalves@ufrpe.br>
2024-07-12 10:45:40 -0300

    tratando erros no 'cadastro.py'

--- src/cadastro.py
+++ src/cadastro.py
@@ -14,19 +14,17 @@ MENSAL_PATH = os.path.join('dados', 'mensal.json')
 def cadastrarConsumoMensal(consumoDiario):
 
     '''
-    Cadastra o consumo mensal 
+        Cadastra o consumo mensal 
 
-    Parâmetros
-    consumoDiario: é o consumo informado pelo usuário, que no caso está sendo gerado automaticamente 
+        Parâmetros
+        consumoDiario: é o consumo informado pelo usuário, que no caso está sendo gerado automaticamente 
 
-    Levanta
-    FileNotFoundError: esse erro ocorre quando ele não encontra o MENSAL_PATH
-    json.JSONDecodeError: no caso dessa função esse erro ocorre quando não se consegue decodificar o arquivo json
-    IOError: ocorrer quando existe um erro ao ler o arquivo
-    e: qualquer tipo de erro
+        Levanta
+        FileNotFoundError: esse erro ocorre quando ele não encontra o MENSAL_PATH
+        json.JSONDecodeError: no caso dessa função esse erro ocorre quando não se consegue decodificar o arquivo json
+        IOError: ocorrer quando existe um erro ao ler o arquivo
     
     '''
-
 
     if not isinstance(consumoDiario, (float)):
         raise ValueError("O consumo diário deve ser float")
@@ -65,9 +63,22 @@ def cadastrarConsumoMensal(consumoDiario):
 
 
 def enviarDadosMensais():
+
+    '''
+        serve para retornar o valor do consumo mensal para a função 'cadastrarConsumo'
+
+        Parâmetros:
+        não tem 
+
+        Levanta
+        FileNotFoundError: esse erro ocorre quando ele não encontra o MENSAL_PATH
+        IOError: ocorrer quando existe um erro ao ler o arquivo     
+
+    '''
+
     try:
         if not os.path.exists(MENSAL_PATH):
-            raise FileNotFoundError(f"O arquivo {MENSAL_PATH} não foi encontrado")
+            raise FileNotFoundError(f'O arquivo {MENSAL_PATH} não foi encontrado')
         
         with open(MENSAL_PATH, 'r') as mensal:
             consumido = json.load(mensal)
@@ -77,11 +88,33 @@ def enviarDadosMensais():
 
         return consumido 
     
+
+    except FileNotFoundError as arquivo_nao_encontrado:
+        print(f'O arquivo {arquivo_nao_encontrado} não foi encontrado')
+        raise
+
+    except IOError as leitura_mal_sucedida:
+        print(f'Leitura do arquivo json foi mal sucedida {leitura_mal_sucedida}')
+        raise
+
     except Exception as e:
         print(f' Um erro aconteceu no {e}')
 
 
 def cadastrarMeta(tarifa, meta, data = date.today()):
+
+    '''
+        serve para cadastrar a meta estabelecida no json respectivo 
+
+        Parâmetros:
+        tarifa: tarifa informada pelo usuario 
+        meta: mmeta informada pelo usuario
+
+        Levanta:
+        FileNotFoundError: esse erro ocorre quando ele não encontra o MENSAL_PATH ou META_PATH
+        json.JSONDecodeError: ocorrer quando existe um erro ao ler o arquivo
+    
+    '''
 
     try:
         if not isinstance(tarifa, float):
@@ -91,7 +124,7 @@ def cadastrarMeta(tarifa, meta, data = date.today()):
             raise ValueError(f'A meta foi informada errada')
         
         if not os.path.exists(MENSAL_PATH):
-            raise FileNotFoundError(f"O arquivo {MENSAL_PATH} não foi encontrado")
+            raise FileNotFoundError(f'O arquivo {MENSAL_PATH} não foi encontrado')
         
         meta_ = {
             'data': data.isoformat(),
@@ -104,20 +137,43 @@ def cadastrarMeta(tarifa, meta, data = date.today()):
 
         with open(META_PATH, 'w') as metas:
             json.dump(meta_, metas, indent= 1)
-        
-    except Exception as e:
-        print(f' Um erro aconteceu no {e}')
 
+    except FileNotFoundError as arquivo_nao_encontrado:
+        print(f'O arquivo {arquivo_nao_encontrado} não foi encontrado')
+        raise
+
+    except json.JSONDecodeError as decoder_mal_sucedida:
+        print(f'não é possivel ler o arquivo json {decoder_mal_sucedida}')
+        raise
+  
+    except Exception as e:
+        print(f'Um erro aconteceu no {e}')
+        raise
 
 def cadastrarConsumo(consumo, data = date.today()):
-    if not isinstance(consumo, float):
-        raise ValueError(f'O consumo deve ser um float')
-    
-    cadastrarConsumoMensal(consumo)
 
-    if data.day != monthrange(data.year, data.month)[1]:
-        gerarRelatorioDiario(consumo)
-    else:
-        gerarRelatorioMensal(enviarDadosMensais)
+    '''
+        Serve para gerar o relatorio diario ou mensal
+
+        Parâmetros:
+        consumo: é o consumo informado pelo usuario 
+    
+    
+    '''
+
+    try:
+        if not isinstance(consumo, float):
+            raise ValueError(f'O consumo deve ser um float')
+        
+        cadastrarConsumoMensal(consumo)
+
+        if data.day != monthrange(data.year, data.month)[1]:
+            gerarRelatorioDiario(consumo)
+        else:
+            gerarRelatorioMensal(enviarDadosMensais)
+    
+    except Exception as e:
+        print(f'Um erro aconteceu no {e}')
+        raise
 
 
